d037c30c98e5b56db0c2ee66dd744bd7af4c132f
Ricardo Salazar <salazar.r31@outlook.es>
2024-09-20 17:49:06 -0500

    hi

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/building-blocks-web/'
+  base: './',
+  build: {
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    }
+}
 });
 
 
