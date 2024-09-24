96188067fe48b89f76a593e416c1fa937cbfbb3e
Ricardo Salazar <salazar.r31@outlook.es>
2024-09-23 21:32:35 -0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,22 @@ import Home from "./Home";
 
 import './App.css';
 
-import rainbow_data from './assets/best-rainbow.json';
-import mono_data from './assets/best-mono.json';
-import both_data from './assets/best.json';
+import rainbow_data from './util/best-rainbow.json';
+import mono_data from './util/best-mono.json';
+import both_data from './util/best.json';
 
-
-export default function App() {
-  return (
-    <Routes>
+/*
+<Routes>
         <Route index element={<Home />} />
         <Route path="rainbow" element={<CubePage data={rainbow_data}/>} />
         <Route path="mono" element={<CubePage data={mono_data}/>} />
         <Route path="both" element={<CubePage data={both_data}/>} />
     </Routes>
+*/
+
+export default function App() {
+  return (
+    <CubePage data={both_data}/>
+    
   );
 }
